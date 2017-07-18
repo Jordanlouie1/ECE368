f2a1a6b9283e27d9216a3690d15e19b92e0d35d2
jordanlouie1 <jordanlouie.1@gmail.com>
2017-07-18 06:35:59 -0400

    binary tree

--- prog3/packing.c
+++ prog3/packing.c
@@ -1,6 +1,48 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-tnode * buildtree(FILE * input){
-  fscanf:wq
-	
+tnode * buildtree(FILE * input){i
+/*	int lines; // lines in file
+
+	while(fgetc(input) != EOF){
+		lines = lines + 1;
+	}
+
+	rewind(input);
+*/
+
+	char c;
+	int lab;
+	double width;
+	double height;
+	double xcoord;
+	double ycoord;
+
+	while((fgetc(input) = c) != EOF){
+		if(c == 'H' || c == 'V'){
+			tnode * newLeaf = malloc(sizeof(tnode));
+			newLeaf->split = c;
+			newLeaf->right = pop(stack);
+			newLeaf->left = pop(stack);
+			stack = push(stack, newLeaf);
+
+		}
+		else{
+			lab = c - '0';
+			fscanf(input, ("(%le,%le)"), width, height);
+			Node * newNode = malloc(sizeof(Node));
+			newNode->label = lab;
+			newNode->width = width;
+			newNode->length = height;
+			stack = push(stack, newNode);
+		}
+		c = fgetc(input);//eof at end???			
+
+	}
+
+}
+
+
+
+
+
